1b15e3a3b99d165e2faa8688b4bb4600a50b1f1d
Aaron <aaron.f.webb@gmail.com>
2017-12-04 11:15:26 -0600

    add project

---
+++ project/bdt.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_gaussian_quantiles
+
