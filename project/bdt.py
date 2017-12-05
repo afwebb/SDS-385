525032fe3234f46511c35abc5598f38b553be119
Aaron <aaron.f.webb@gmail.com>
2017-12-05 10:40:06 -0600

    read in data

--- project/bdt.py
+++ project/bdt.py
@@ -1,7 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import sklearn as sk
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import make_gaussian_quantiles
 
+def read_file(f, i, batch_size):
+    X,y = sk.datasets.load_svmlight_file(f)# offset=i*batch_size, length=batch_size)
+    X = X.tocsr()
+    return X, y
+
+X, y = read_file("data", 0, 10)
+
+#X = sk.preprocessing.normalize(X, norm='l1', axis=1)
+print X
